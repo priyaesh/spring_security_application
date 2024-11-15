42e621da8d4a456542fa7381103586f6ed89d630
Hemapriya <hemapriyasampath@gmail.com>
2024-11-15 11:37:44 -0500

    generating csrf tokens

--- src/main/java/com/spring_security/demo/controller/WelcomeController.java
+++ src/main/java/com/spring_security/demo/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.spring_security.demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,10 @@ public class WelcomeController {
     public String welcome(){
         return "Welcome to spring security";
     }
+    @GetMapping("/csrf")
+    public CsrfToken getToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
 
+
+    }
 }

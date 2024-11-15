20bd8d07ec925483547400036a98788a910621a2
Hemapriya <hemapriyasampath@gmail.com>
2024-11-15 15:13:28 -0500

    inMemoryUserDetailsManager

--- src/main/java/com/spring_security/demo/config/WebSecurityConfig.java
+++ src/main/java/com/spring_security/demo/config/WebSecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,6 +23,22 @@ public class WebSecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails priyaEsh
+                = User.withUsername("priyaEsh")
+                .password("{noop}admin@123")
+                .roles("USER")
+                .build();
+
+        UserDetails Esh
+                = User.withUsername("Esh")
+                .password("admin@123")
+                .roles("USER")
+                .build();
+        return  new InMemoryUserDetailsManager(priyaEsh,Esh);
 
     }
 }

bb331a5c16161df0f6f1c9a2cc91f09ff30c8e00
Hemapriya <hemapriyasampath@gmail.com>
2024-11-15 13:06:30 -0500

    getting back all the authentication

---
+++ src/main/java/com/spring_security/demo/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.spring_security.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+
+                return httpSecurity.build();
+
+    }
+}
